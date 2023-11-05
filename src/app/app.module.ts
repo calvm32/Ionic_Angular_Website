13f071f0845cffaf86915cb1771f3d776a8347ab
calvm32 <cal.muon32@gmail.com>
2023-11-05 13:28:54 -0600

    trying hash thingy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
+import {HashLocationStrategy, Location, LocationStrategy} from '@angular/common';
+import {Component} from '@angular/core';
 
 @NgModule({
 declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ],
   bootstrap: [AppComponent],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  exports: [],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy, }],
+  exports: [RouterModule],
 
 })
 
 export class AppModule { }
+
+@Component({
+  selector: 'hash-location',
+  providers: [Location, {provide: LocationStrategy, useClass: HashLocationStrategy}],
+  template: ``
+})
+export class HashLocationComponent {
+  location: Location;
+  constructor(location: Location) {
+    this.location = location;
+  }
+}
